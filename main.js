09c7407e27243ae43d05b58ecab05d3322946e45
unknown <satyenderprajapati413@gmail.com>
2024-11-04 16:32:00 +0530

    introduction to timeline

--- main.js
+++ main.js
@@ -47,48 +47,89 @@ import gsap from "gsap";
 //   }
 // );
 
-const randomColor = () => {
-  let str = "#";
-  const colorArray = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-  ];
+// const randomColor = () => {
+//   let str = "#";
+//   const colorArray = [
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//   ];
 
-  for (let i = 1; i <= 6; i++) {
-    str += colorArray[Math.floor(Math.random() * colorArray.length)];
-  }
+//   for (let i = 1; i <= 6; i++) {
+//     str += colorArray[Math.floor(Math.random() * colorArray.length)];
+//   }
 
-  return str;
-};
+//   return str;
+// };
 
-gsap.to(
-  {},
+// gsap.to(
+//   {},
+//   {
+//     duration: 5,
+//     repeat: -1,
+
+//     onUpdate: function () {
+//       const angle = Date.now() / 1000;
+//       const radius = 200;
+//       console.log(radius * Math.sin(angle));
+//       gsap.set(".box1", {
+//         x: radius * Math.sin(angle),
+//         y: radius * Math.cos(angle),
+//         background: randomColor,
+//       });
+//     },
+//   }
+// );
+
+// gsap.to(".box2", {
+//   x: 200,
+//   duration: 5,
+// });
+
+// gsap.to(".box3", {
+//   x: 300,
+//   duration: 5,
+//   delay: 5,
+// });
+
+const t1 = gsap.timeline();
+const btn = document.querySelector(".btn");
+
+t1.to(
+  ".box2",
   {
-    duration: 5,
-    repeat: -1,
-
-    onUpdate: function () {
-      const angle = Date.now() / 1000;
-      const radius = 200;
-      gsap.set(".box1", {
-        x: radius * Math.cos(angle),
-        y: radius * Math.sin(angle),
-        background: randomColor,
-      });
-    },
-  }
+    x: 100,
+    duration: 15,
+    ease: "none",
+  },
+  "same"
+).to(
+  ".box3",
+  {
+    x: 200,
+    duration: 15,
+    rotation: 360,
+    ease: "none",
+  },
+  "same"
 );
+
+btn.addEventListener("click", () => {
+  // t1.pause();
+  // t1.timeScale(12);
+  console.log(t1.progress());
+  t1.progress(0.7);
+});
