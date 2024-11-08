eaf8be298d855e9e612f6176b26e97e83f47abbc
unknown <satyenderprajapati413@gmail.com>
2024-11-08 15:33:35 +0530

    created a loading anim

--- main.js
+++ main.js
@@ -105,31 +105,164 @@ import gsap from "gsap";
 //   delay: 5,
 // });
 
+// const t1 = gsap.timeline();
+// const btn = document.querySelector(".btn");
+
+// t1.to(".box2", {
+//   x: 300,
+//   duration: 5,
+//   ease: "none",
+// })
+//   .to(
+//     ".box4",
+//     {
+//       x: 400,
+//       duration: 5,
+//       ease: "none",
+//     },
+//     "-=10"
+//   )
+//   .to(
+//     ".box3",
+//     {
+//       x: 600,
+//       duration: 5,
+//       rotation: 360,
+//       ease: "none",
+//     },
+//     "-=10"
+//   );
+
+// btn.addEventListener("click", () => {
+//   // t1.pause();
+//   // t1.timeScale(12);
+//   console.log(t1.seek());
+//   t1.seek(4);
+// });
+
+// gsap.to(
+//   {},
+//   {
+//     duration: 5,
+//     repeat: -1,
+//     onUpdate: function () {
+//       const angle = Date.now() / 1000;
+//       console.log(100 * Math.cos(angle));
+
+//       gsap.set(".box2", {
+//         x: 100 * Math.cos(angle),
+//         y: 100 * Math.sin(angle),
+//       });
+//     },
+//   }
+// );
+
+// const checkPrefix = gsap.utils.checkPrefix("filter");
+
+// console.log(checkPrefix);
+
+// const clamp = gsap.utils.clamp(0, 100);
+// console.log(clamp);
+// console.log(clamp(200));
+// console.log(clamp(-150));
+// console.log(clamp(75));
+
+// const end = gsap.utils.distribute({
+//   base: "5vh",
+//   amount: "50vh",
+//   ease: "power3.in",
+//   from: "center",
+// });
+
+// console.log(end());
+
+// gsap.to(".bar", {
+//   height: end,
+// });
+
+// window.addEventListener("click", () => {
+//   const t1 = gsap.timeline();
+//   t1.to(".bar", {
+//     height: "100vh",
+//     duration: 2,
+//     ease: "bounce.in",
+//   });
+
+//   t1.to(
+//     ".bar",
+//     {
+//       width: "100%",
+//       duration: 1,
+//       ease: "power4.inOut",
+//     },
+//     "same"
+//   );
+
+//   t1.to(
+//     ".container",
+//     {
+//       gap: "0",
+//       width: "100%",
+//       duration: 1,
+//       ease: "power4.inOut",
+//     },
+//     "same"
+//   );
+
+//   t1.to(
+//     ".hero-heading",
+//     {
+//       color: "#000",
+//       duration: 1,
+//       ease: "power4.inOut",
+//     },
+//     "same"
+//   );
+// });
+
 const t1 = gsap.timeline();
-const btn = document.querySelector(".btn");
 
-t1.to(
-  ".box2",
-  {
-    x: 100,
-    duration: 15,
-    ease: "none",
-  },
-  "same"
-).to(
-  ".box3",
-  {
-    x: 200,
-    duration: 15,
-    rotation: 360,
-    ease: "none",
-  },
-  "same"
-);
-
-btn.addEventListener("click", () => {
-  // t1.pause();
-  // t1.timeScale(12);
-  console.log(t1.progress());
-  t1.progress(0.7);
+t1.to(".row", {
+  translateX: "-50%",
+  duration: 3,
+  stagger: 0.25,
+  repeat: -1,
+  yoyo: true,
+  ease: "back.in",
 });
+
+setTimeout(() => {
+  t1.pause();
+
+  const t2 = gsap.timeline();
+
+  t2.to(".container", {
+    scale: 3,
+    rotation: 90,
+    duration: 2,
+    ease: "power4.inOut",
+  });
+
+  t2.to(".row", {
+    translateX: "-550%",
+    duration: 1,
+    stagger: 0.1,
+    ease: "power3.inOut",
+  });
+  t2.to(
+    ".container",
+    {
+      translateY: "-500%",
+      duration: 3,
+      ease: "power4.inOut",
+    },
+    "-=1"
+  );
+
+  t2.to(".hero-heading", {
+    top: "40%",
+    opacity: 1,
+    ease: "power3.inOut",
+    duration: 1,
+  });
+}, 15000);
